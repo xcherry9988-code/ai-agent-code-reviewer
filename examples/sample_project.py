008cb5e38409ee48d53f422ced1ef64462ee9431
Cherrui <xcherry9988@gmail.com>
2026-05-06 15:58:47 +0800

    Add calculate and very_long_function to sample_project
    Added a calculate function with error handling and a very long function to generate a list.

---
+++ examples/sample_project.py
@@ -0,0 +1,17 @@
+# TODO: optimize this function
+
+def calculate(a, b):
+    try:
+        return a / b
+
+    except:
+        return 0
+
+
+def very_long_function():
+    data = []
+
+    for i in range(300):
+        data.append(i)
+
+    return data
